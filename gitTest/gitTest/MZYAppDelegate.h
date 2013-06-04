6cc315e4dd0a27b88b749a695ebe072a5146887b
nightmare <nightmare@maziyuan.local>
2013-06-04 11:49:05 +0800

    init

---
+++ gitTest/gitTest/MZYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MZYAppDelegate.h
+//  gitTest
+//
+//  Created by nightmare on 13-6-4.
+//  Copyright (c) 2013年 nightmare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MZYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTest/gitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by nightmare on 13-6-4.
+//  Copyright (c) 2013年 nightmare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MZYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MZYAppDelegate class]));
+    }
+}
